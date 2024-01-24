bc5261de8887e79e101b79ecda2670070dbfe8a5
MPerskawiec <mariusz.perskawiec@gmail.com>
2024-01-24 13:59:28 +0100

    Adding Philosopher class

---
+++ philosopher.cpp
@@ -0,0 +1,43 @@
+#include "philosopher.h"
+
+void Philosopher::work() {
+    while (1) {
+        PhState_ = PhilosopherState::WaitingForForks;
+        {
+            std::cout << id_ << ": czekam za widelcem \n";
+            std::scoped_lock lock{leftFork_, rightFork_};
+            std::cout << id_ << ": jem \n";
+            eating();
+        }
+        std::cout << id_ << ": odpoczywam \n";
+        resting();
+    }
+}
+
+size_t Philosopher::getId() const {
+    return id_;
+}
+
+PhilosopherState Philosopher::getPhState() const {
+    return PhState_;
+}
+
+void Philosopher::resting() {
+    PhState_ = PhilosopherState::Resting;
+    auto time = randTime();
+    std::this_thread::sleep_for(std::chrono::seconds(time));
+}
+
+void Philosopher::eating() {
+    PhState_ = PhilosopherState::Eating;
+    auto time = randTime();
+    std::this_thread::sleep_for(std::chrono::seconds(time));
+}
+
+size_t Philosopher::randTime() {
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<int> distribution(2, 10);
+
+    return distribution(generator);
+}
---
+++ philosopher.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <random>
+#include <thread>
+
+enum class PhilosopherState {
+    Idle,
+    Resting,
+    WaitingForForks,
+    Eating
+};
+
+class Philosopher {
+public:
+    Philosopher(size_t id, std::mutex& leftFork, std::mutex& rightFork)
+        : id_(id), leftFork_(leftFork), rightFork_(rightFork), PhState_(PhilosopherState::Idle) {}
+
+    void work();
+
+    size_t getId() const;
+    PhilosopherState getPhState() const;
+
+private:
+    size_t id_;
+    std::mutex& leftFork_;
+    std::mutex& rightFork_;
+    PhilosopherState PhState_;
+
+    void resting();
+    void eating();
+    size_t randTime();
+};
